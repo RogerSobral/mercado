06967130c01094ffc03a349437c1573cfc06e158
RogerioSobral <47829562+RogerSobral@users.noreply.github.com>
2025-01-31 17:36:44 -0300

    modi

--- markting/src/components/MenuPrincipal.jsx
+++ markting/src/components/MenuPrincipal.jsx
@@ -1,18 +1,27 @@
-// src/components/MenuPrincipal.jsx
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Produtos from './Produtos';
 import Fornecedores from './Fornecedores';
 import Compras from './Compras';
 import Relatorios from './Relatorios';
-import LoginForm from './LoginForm'
+import LoginForm from './LoginForm';
 import './MenuPrincipal.css'; // Importando o CSS para estilização
 
 const MenuPrincipal = () => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
   return (
     <Router>
       <div className="menu-principal">
-        <nav className="sidebar">
+        <button className="sidebar-toggle" onClick={toggleSidebar}>
+          {/* Ícone de hambúrguer */}
+          ☰
+        </button>
+        <nav className={`sidebar ${isSidebarOpen ? 'open' : ''}`}>
           <ul>
             <li>
               <Link to="/produtos">Produtos</Link>
@@ -27,7 +36,7 @@ const MenuPrincipal = () => {
               <Link to="/relatorios">Relatórios</Link>
             </li>
             <li>
-            <Link>Sair</Link>
+              <Link>Sair</Link>
             </li>
           </ul>
         </nav>
@@ -37,7 +46,6 @@ const MenuPrincipal = () => {
             <Route path="/fornecedores" element={<Fornecedores />} />
             <Route path="/compras" element={<Compras />} />
             <Route path="/relatorios" element={<Relatorios />} />
-            
             <Route path="/" element={<Produtos />} /> {/* Página padrão */}
           </Routes>
         </div>
